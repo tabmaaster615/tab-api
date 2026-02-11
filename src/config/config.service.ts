a8391522f6f8f0c380434e5f3e36a5e67760a2d2
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-02-11 23:39:25 +0600

    added all the config

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -14,8 +14,19 @@ import authConfig from './auth.config';
       validationSchema: Joi.object({
         APP_NAME: Joi.string().required(),
         NODE_ENV: Joi.string()
-        .valid('development', 'production', 'test').required(),
+          .valid('development', 'production', 'test')
+          .required(),
         PORT: Joi.number().default(3000),
+        API_PREFIX: Joi.string().default('api'),
+
+        DATABASE_URL: Joi.string().required(),
+        DATABASE_SSL: Joi.boolean().default(false),
+
+        JWT_ACCESS_SECRET: Joi.string().required(),
+        JWT_ACCESS_EXPIRES_IN: Joi.string().default('15m'),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_REFRESH_EXPIRES_IN: Joi.string().default('3d'),
+      }),
     }),
   ],
 })
