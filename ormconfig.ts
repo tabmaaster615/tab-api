a1a09e97e6ce9f1f14ec31e1f5f753d043dd9161
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-02-13 23:28:19 +0600

    added the ormconfig.ts file

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  entities: [join(__dirname, '**/*.entity.{ts,js}')],
+  migrations: [join(__dirname, 'migrations/*{.ts,.js}')],
+
+  // Safety first
+  synchronize: false,
+  logging: process.env.NODE_ENV === 'development',
+});
